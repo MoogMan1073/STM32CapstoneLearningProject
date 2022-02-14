cffc027df5dee8d510b3ba02effa3aeaf93c9df3
Eli Moorhouse <elimoorhouse01@gmail.com>
2022-02-13 19:36:46 -0500

    Added infrastructure to read in and store calibration data from BMP085
    Fixed BMP085 constructor as well.

--- CapstoneLearningProject/Core/Src/Classes/BMP085.cpp
+++ CapstoneLearningProject/Core/Src/Classes/BMP085.cpp
@@ -7,15 +7,70 @@
 
 #include "BMP085.h"
 
-BMP085::BMP085()
+BMP085::BMP085(I2C_HandleTypeDef* hi2c)
 {
-	// TODO Auto-generated constructor stub
+	_hi2c = hi2c;
 
 }
+
+/**
+ * @brief Most sensor readings in the MPU6050 are 16-bit. This function can be used to take in
+ * data from two 8-bit registers and combine it into 1 16-bit value
+ * @param high_reg	register holding the more significant 8 bits of the 16-bit reading
+ * @param low_reg	register holding the less significant 8 bits of the 16-bit reading
+ * @return	derived 16-bit value from combining the data in the two registers
+ */
+uint16_t BMP085::Read16BitValue(uint8_t high_reg, uint8_t low_reg)
+{
+	// Tell MPU6050 that we want to read from the temperature register
+	_buf[0] = high_reg;
+	_buf[1] = low_reg;
+	_ret = HAL_I2C_Master_Transmit(_hi2c, BMP085_8BIT_ADDRES, _buf, 1, BMP085_MAX_TIMEOUT);
+	if ( _ret != HAL_OK )
+	{
+	  strcpy((char*)_buf, "Error Tx\r\n");
+	  _val = BMP085_ERROR_CODE;
+	}
+	else
+	{
+		// Read 2 bytes from the temperature register
+		_ret = HAL_I2C_Master_Receive(_hi2c, BMP085_8BIT_ADDRES, _buf, 2, BMP085_MAX_TIMEOUT);
+		if (_ret != HAL_OK )
+		{
+			strcpy((char*)_buf, "Error Rx\r\n");
+			_val = BMP085_ERROR_CODE;
+		}
+
+		else
+		{
+			// Retrieve temp data from buffer
+			_val = ((uint16_t)_buf[0] << BMP085_HIGH_REGISTER_OFFSET | _buf[1]);
+		}
+	}
+
+		return _val;
+}
+
 
 void BMP085::getCalData()
 {
 	// collect all calibration data stored on BMP085 EEPROM
+	 _ac1 = Read16BitValue(BMP085_RA_AC1_H,	BMP085_RA_AC1_L);
+	 _ac2 = Read16BitValue(BMP085_RA_AC2_H, BMP085_RA_AC2_L);
+	 _ac3 = Read16BitValue(BMP085_RA_AC3_H, BMP085_RA_AC3_L);
+	 _ac4 = Read16BitValue(BMP085_RA_AC4_H, BMP085_RA_AC4_L);
+	 _ac5 = Read16BitValue(BMP085_RA_AC5_H, BMP085_RA_AC5_L);
+	 _ac6 = Read16BitValue(BMP085_RA_AC6_H, BMP085_RA_AC6_L);
+	 _b1 = Read16BitValue(BMP085_RA_B1_H, BMP085_RA_B1_L);
+	 _b2 = Read16BitValue(BMP085_RA_B2_H, BMP085_RA_B2_L);
+	 _mb = Read16BitValue(BMP085_RA_MB_H, BMP085_RA_MB_L);
+	 _mc = Read16BitValue(BMP085_RA_MC_H, BMP085_RA_MC_L);
+	 _md = Read16BitValue(BMP085_RA_MD_H, BMP085_RA_MD_L);
+}
+
+void BMP085::Init()
+{
+
 }
 
 float BMP085::readTemperature()
--- CapstoneLearningProject/Core/Src/Classes/BMP085.h
+++ CapstoneLearningProject/Core/Src/Classes/BMP085.h
@@ -6,18 +6,72 @@
  */
 
 #ifndef SRC_BMP085_H_
+
+#include "string.h"
+
+#include "stm32f030x8.h"
+#include "stm32f0xx_hal.h"
+
 #define SRC_BMP085_H_
 
 #define BMP085_DEFAULT_ADDRESS 0x77
 #define BMP085_8BIT_ADDRES BMP085_DEFAULT_ADDRESS << 1
+#define BMP085_RA_AC1_H 0xAA
+#define BMP085_RA_AC1_L 0xAB
+#define BMP085_RA_AC2_H 0xAC
+#define BMP085_RA_AC2_L 0xAD
+#define BMP085_RA_AC3_H 0xAE
+#define BMP085_RA_AC3_L 0xAF
+#define BMP085_RA_AC4_H 0xB0
+#define BMP085_RA_AC4_L 0xB1
+#define BMP085_RA_AC5_H 0xB2
+#define BMP085_RA_AC5_L 0xB3
+#define BMP085_RA_AC6_H 0xB4
+#define BMP085_RA_AC6_L 0xB5
+#define BMP085_RA_B1_H 0xB6
+#define BMP085_RA_B1_L 0xB7
+#define BMP085_RA_B2_H 0xB8
+#define BMP085_RA_B2_L 0xB9
+#define BMP085_RA_MB_H 0xBA
+#define BMP085_RA_MB_L 0xBB
+#define BMP085_RA_MC_H 0xBC
+#define BMP085_RA_MC_L 0xBD
+#define BMP085_RA_MD_H 0xBE
+#define BMP085_RA_MD_L 0xBF
+
+#define BMP085_BUFFER_SIZE 32
+#define BMP085_HIGH_REGISTER_OFFSET 8
+#define BMP085_MAX_TIMEOUT 5000
+#define BMP085_ERROR_CODE 0xFFFF
 
 #define TEMP_READ_WAIT_TIME_MS 10 // Datasheet states to wait at least 4.5 ms
 
 class BMP085 {
 public:
-	BMP085();
+	BMP085(I2C_HandleTypeDef* hi2c);
+	uint16_t Read16BitValue(uint8_t low_reg, uint8_t high_reg);
 	void getCalData();
+	void Init();
 	float readTemperature();
+
+private:
+	uint8_t _buf[BMP085_BUFFER_SIZE] = {};
+	uint16_t _val;
+	I2C_HandleTypeDef* _hi2c;
+	HAL_StatusTypeDef _ret;
+
+	// Calibration Data stored in EEPROM
+	uint16_t _ac1;
+	uint16_t _ac2;
+	uint16_t _ac3;
+	uint16_t _ac4;
+	uint16_t _ac5;
+	uint16_t _ac6;
+	uint16_t _b1;
+	uint16_t _b2;
+	uint16_t _mb;
+	uint16_t _mc;
+	uint16_t _md;
 };
 
 #endif /* SRC_BMP085_H_ */
